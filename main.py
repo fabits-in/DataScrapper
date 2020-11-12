8fcbee842d85702e2d4c45605398bd1fcf6e5e0c
anoop <anoophky@gmail.com>
2020-11-12 21:47:43 +0530

    nse api

--- main.py
+++ main.py
@@ -1,20 +1,55 @@
-import requests
+import nse
 
-headers = {
-    'authority': 'www.nseindia.com',
-    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36 OPR/72.0.3815.207',
-    'accept': '*/*',
-    'sec-fetch-site': 'same-origin',
-    'sec-fetch-mode': 'cors',
-    'sec-fetch-dest': 'empty',
-    'referer': 'https://www.nseindia.com/get-quotes/equity?symbol=SBIN',
-    'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    'cookie': 'nsit=yr2nbX9tttXqbVdhJsMUIMir; '
-              'nseQuoteSymbols=[{"symbol":"SBIN","identifier":null,"type":"equity"}];'
-              'nseappid=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJhcGkubnNlIiwiYXVkIjoiYXBpLm5zZSIsImlhdCI6MTYwNDk5OTY3MiwiZXhwIjoxNjA1MDAzMjcyfQ.tU0bewmoSUMIS5PeYI4OqSWMw7sg8hKLSuPcZOJsnTE; '
+instrument_info = nse.equity_info("SBIN")
+instrument_trade_info = nse.equity_trade_info("SBIN")
 
-}
+print(instrument_info)
+print(instrument_trade_info)
 
-response = requests.get('https://www.nseindia.com/api/quote-equity?symbol=SBIN&section=trade_info', headers=headers)
-
-print(response.text)
+#
+# "symbol"
+# "isin"
+# "status"
+# "date of listing"
+# "Industry"
+# "P/E"
+# "Sectoral_Index P/E"
+# "Sectoral_Index"
+# "Board Status"
+# "Trading Status"
+# "Trading Segment"
+# "Session No."
+# "SLB"
+# "Class of Shares"
+# "Derivatives"
+# "Face Value"
+# "List Issued Capital"
+# "Surveillance"
+# "Total Market Cap"
+# "Free Float Market Cap"
+# "Impact cost"
+# -----
+# "PREV. CLOSE"
+# "OPEN"
+# "HIGH"
+# "LOW"
+# "CLOSE"
+# "VWAP"
+# "LOWER BAND"
+# "UPPER BAND"
+# "PRICE BAND"
+# "ADJUSTED PRICE"
+# "Traded Volume"
+# "Traded Value"
+# "Value at Risk"
+# "Security VaR"
+# "Index VaR"
+# "VaR Margin"
+# "Extreme Loss Rate"
+# "Adhoc Margin"
+# "Applicable Margin Rate"
+# ----
+# "52_OPEN"
+# "52_HIGH"
+# "52_LOW"
+# "52_CLOSE"
---
+++ nse/__init__.py
@@ -0,0 +1,26 @@
+import requests
+
+headers = {
+    'authority': 'www.nseindia.com',
+    'upgrade-insecure-requests': '1',
+    'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.183 Safari/537.36 OPR/72.0.3815.320',
+    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
+    'sec-fetch-site': 'none',
+    'sec-fetch-mode': 'navigate',
+    'sec-fetch-user': '?1',
+    'sec-fetch-dest': 'document',
+    'accept-language': 'en-GB,en;q=0.9',
+}
+
+nse = requests.Session()
+nse.get("https://www.nseindia.com/", headers=headers)
+
+
+def equity_trade_info(symbol):
+    response = nse.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}&section=trade_info', headers=headers)
+    return response.text
+
+
+def equity_info(symbol):
+    response = nse.get(f'https://www.nseindia.com/api/quote-equity?symbol={symbol}', headers=headers)
+    return response.text
